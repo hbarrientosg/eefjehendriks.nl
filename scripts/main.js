8255801441819f94b40da339d987160ad8037e35
Humberto <Hawksmoor@gmail.com>
2014-04-27 00:41:06 +0100

    Update Sliders.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+(function(slidr){
+	slidr.create('slides-container',{
+		  after: function(e) { console.log('in: ' + e.in.slidr); },
+		  before: function(e) { console.log('out: ' + e.out.slidr); },
+		  breadcrumbs: true,
+		  controls: 'none',
+		  direction: 'horizontal',
+		  fade: false,
+		  keyboard: true,
+		  overflow: true,
+		  theme: '#222',
+		  timing: { 'fade': '0.5s ease-in' },
+		  touch: true,
+		  transition: 'fade'
+	}).auto()
+
+})(window.slidr)
