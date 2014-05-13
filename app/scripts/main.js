fd79f61774e2ebbdc7f11ae024339110ead563b8
Humberto <Hawksmoor@gmail.com>
2014-05-13 19:01:48 +0100

    Update Project Selection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,57 @@
-console.log('\'Allo \'Allo!');
+(function() {
+
+    var MenuItem = function MenuItem() {};
+    MenuItem.prototype = {
+        constructor: MenuItem,
+        load: function ( src ) {
+            $('.container').load(src)
+        },
+        onClick: function (event) {
+            var $this = $(this);
+
+            $this.parent('.nav').find('.active').removeClass('active');
+            $this.addClass('active');
+
+            MenuItem.prototype.load($this.data('src'));
+        }
+    }
+
+    function splitColumns() {
+        var winWidth = $(window).width(),
+            columnNumb = 1;
+
+        if (winWidth > 1024) {
+            columnNumb = 4;
+        } else if (winWidth > 900) {
+            columnNumb = 2;
+        } else if (winWidth > 479) {
+            columnNumb = 2;
+        } else if (winWidth < 479) {
+            columnNumb = 1;
+        }
+
+        return columnNumb;
+    }
+
+    function setColumns() {
+        var winWidth = $(window).width(),
+            columnNumb = splitColumns(),
+            postWidth = Math.floor(winWidth / columnNumb);
+
+        $('.portfolio').find('.portfolio-item').each(function () {
+            $(this).css( {
+                width : postWidth + 'px'
+            });
+        });
+    }
+
+    function setProjects() {
+        setColumns();
+    }
+
+    $(document)
+        .on('click','header .nav a', MenuItem.prototype.onClick);
+    $(window)
+        .on('resize', setProjects);
+
+})(window.jQuery);
