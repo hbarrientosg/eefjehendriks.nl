8f5ee44bcd9eb7f5d2bb5edc6d8af4a27cf42652
Humberto <Hawksmoor@gmail.com>
2014-05-15 21:31:01 +0100

    Added Carrousell

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,8 +3,8 @@
     var MenuItem = function MenuItem() {};
     MenuItem.prototype = {
         constructor: MenuItem,
-        load: function ( src ) {
-            $('.container').load(src)
+        load: function ( src, callback ) {
+            $('.container').load(src, callback)
         },
         onClick: function (event) {
             var $this = $(this);
@@ -12,7 +12,14 @@
             $this.parent('.nav').find('.active').removeClass('active');
             $this.addClass('active');
 
-            MenuItem.prototype.load($this.data('src'));
+            MenuItem.prototype.load($this.data('src'), function() {
+                $('div.container .personal-box').onScreen({
+                    container: '.personal-wrap',
+                    direction: 'horizontal',
+                    toggleClass: 'show',
+                    tolerance: 50
+                });
+            });
         }
     }
 
@@ -50,7 +57,23 @@
     }
 
     $(document)
-        .on('click','header .nav a', MenuItem.prototype.onClick);
+        .on('click','header .nav a', MenuItem.prototype.onClick)
+        .ready(function() {
+            $.backstretch([
+                "/images/panorama/buerchenMystik1.2x.jpg"
+              , "/images/panorama/buerchenMystik2.2x.jpg"
+              , "/images/panorama/buerchenMystik3.2x.jpg"
+              , "/images/panorama/drawing1.2x.jpg"
+              , "/images/panorama/drawing2.2x.jpg"
+              , "/images/panorama/drawing3.2x.jpg"
+              , "/images/panorama/graduationProject1.2x.jpg"
+              , "/images/panorama/graduationProject2.2x.jpg"
+              , "/images/panorama/graduationProject3.2x.jpg"
+            ], {duration: 3000, fade: 750});
+
+            //$('.nav .active').trigger('click');
+        });
+
     $(window)
         .on('resize', setProjects);
 
