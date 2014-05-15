8fd2a70cdbc386ba421e3f5f7f49189568369c90
Humberto <Hawksmoor@gmail.com>
2014-05-15 23:18:36 +0100

    update main js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -61,15 +61,15 @@
         .on('click','header .nav a', MenuItem.prototype.onClick)
         .ready(function() {
             $.backstretch([
-                "/images/panorama/buerchenMystik1.2x.jpg"
-              , "/images/panorama/buerchenMystik2.2x.jpg"
-              , "/images/panorama/buerchenMystik3.2x.jpg"
-              , "/images/panorama/drawing1.2x.jpg"
-              , "/images/panorama/drawing2.2x.jpg"
-              , "/images/panorama/drawing3.2x.jpg"
-              , "/images/panorama/graduationProject1.2x.jpg"
-              , "/images/panorama/graduationProject2.2x.jpg"
-              , "/images/panorama/graduationProject3.2x.jpg"
+                "images/panorama/buerchenMystik1.2x.jpg"
+              , "images/panorama/buerchenMystik2.2x.jpg"
+              , "images/panorama/buerchenMystik3.2x.jpg"
+              , "images/panorama/drawing1.2x.jpg"
+              , "images/panorama/drawing2.2x.jpg"
+              , "images/panorama/drawing3.2x.jpg"
+              , "images/panorama/graduationProject1.2x.jpg"
+              , "images/panorama/graduationProject2.2x.jpg"
+              , "images/panorama/graduationProject3.2x.jpg"
             ], {duration: 3000, fade: 750});
 
             //$('.nav .active').trigger('click');
